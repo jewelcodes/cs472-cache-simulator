a520b0eedbbc3f72beb598ff2864da6d6b12ef29
jewelcodes <omarmelghoul01@gmail.com>
2024-02-26 17:59:27 -0500

    cache constructor

---
+++ src/cache.cpp
@@ -0,0 +1,20 @@
+/*
+ * MET CS 472 - Computer Architecture
+ * Project 2: Cache Simulator
+ * Omar Elghoul
+ */
+
+#include <cache.h>
+
+/* CONSTRUCTOR */
+Cache::Cache(PhysicalMemory memory, size_t entryCount) {
+    this->memory = memory;
+    this->entryCount = entryCount;
+    this->entry.reserve(entryCount);
+}
+
+CacheEntry::CacheEntry() {
+    this->valid = false;    // none of the other fields matter if we start with valid=false
+                            // so we don't need to manually initialize them
+}
+
