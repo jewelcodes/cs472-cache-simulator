802ffb63b6d3d4a00fca7acd2b0c88124372fa62
jewelcodes <omarmelghoul01@gmail.com>
2024-03-11 18:42:45 -0400

    test for writeback cache

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,18 @@ int main(int argc, char **argv) {
     Cache cache(&memory, BLOCK_COUNT);
 
     // test
-    cache.read(0x7A2);
+    /*cache.read(0x7A2);
     cache.read(0x2E);
     cache.read(0x2F);
     cache.read(0x3D5);
-    cache.write(0x7A2, 0x88);
+    cache.write(0x7A2, 0x88);*/
+
+    //cache.display();
+
+    cache.write(0x150, 0xF3);
+    cache.write(0x155, 0x7D);
+    cache.read(0x250);
+    cache.read(0x150);
     cache.display();
 
     return 0;
