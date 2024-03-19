ae7fe898428ff8755841a1813fe09f1a475a1742
jewelcodes <omarmelghoul01@gmail.com>
2024-03-18 22:29:10 -0400

    scripting: writes, displays, and pauses

--- src/script.cpp
+++ src/script.cpp
@@ -23,6 +23,8 @@ void Script::execute(string filename) {
     ifstream file(filename, ios::in);
     string line;
     uint32_t address;
+    uint8_t value;
+    bool pause = false;     // whether we will pause after every instruction
 
     while(getline(file, line)) {
         if(line[0] == 'd') {
@@ -30,6 +32,25 @@ void Script::execute(string filename) {
         } else if(line[0] == 'r') {
             address = stoi(&line[2], 0, 16);
             this->cache->read(address);
+        } else if(line[0] == 'w') {
+            address = stoi(&line[2], 0, 16);
+
+            if(!getline(file, line)) {
+                cout << "unexpected end of file" << endl;
+                return;
+            }
+
+            value = stoi(line, 0, 16);
+            this->cache->write(address, value);
+        } else if(line[0] == 'p') {
+            // this isn't part of the assignment but will make a recording of the submission easier!
+            pause = !pause;
+            if(pause) cout << "press enter after each operation to proceed to the next" << endl;
+        }
+
+        if(pause && line[0] != 'p') {
+            //cout << " (press enter to continue) ";
+            cin.get();
         }
     }
 }
